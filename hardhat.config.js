c019e7bc1ca35c8b0d3190826a65ff4a0df41177
yash jain <yashjain07.yj@gmail.com>
2023-04-28 13:48:14 +0530

    Made The Deploy Script To Deploy Our Smart Contract On Polygon Network And Also Changed Our hadhat.config File.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
+const polygonURL = process.env.PLOYGON_URL;
+
+const account = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.17",
+
+  networks : {
+
+    mumbai : {
+
+      url : polygonURL,
+      accounts : [account]
+
+    }
+
+  }
+
 };
