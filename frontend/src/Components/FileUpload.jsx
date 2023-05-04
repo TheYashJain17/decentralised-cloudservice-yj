ff816aba0cdf079c0086c34fb0f78080f678bb1c
yash jain <yashjain07.yj@gmail.com>
2023-05-04 13:37:16 +0530

    Made HandleSubmit Function To Upload The Image On IPFS Network.

--- frontend/src/Components/FileUpload.jsx
+++ frontend/src/Components/FileUpload.jsx
@@ -1,12 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import axios from 'axios';
 
+import './CSSFiles/FileUpload.css';
+
 const FileUpload = ({contract , account , provider}) => {
+
+  const [file , setFile] = useState(null);
+
+  const [fileName , setFileName] = useState("No Image Selected");
+
+  const handleSubmit = async(event) => {
+
+    event.preventDefault();
+
+    if(file){
+
+      try {
+
+        const formData = new FormData();
+
+        formData("File" , file)
+
+        const resultedFile = await axios({
+
+          method: "post",
+          url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+          data : formData,
+          headers : {
+
+            pinataApiKey: "afffda434230e6aed6bc",
+
+            pinataSecretApiKey: "707166a1a06ac9e751d49489a675b5ce1ae677261e40fb8b957c7f8d7d0f715a",
+
+            "Content-Type": "multipart/form-data"
+
+          }
+        })
+
+        const imageHash = `ipfs://${resultedFile.data.IpfsHash}`
+
+        await contract.addYourImage(imageHash);
+
+        alert("Image Uploaded Successfully");
+
+        setFileName("No Image Selected");
+
+        setFile(null);
+
+        
+      } catch (error) {
+
+        alert(error.name)
+        
+      }
+
+    }
+
+  }
+
+const retrieveFile = () => {
+
+
+
+}
+
   return (
     <div className='top'>
     
-    
+    <form className='form' onSubmit={handleSubmit}>
+
+    <label htmlFor="file-upload" className='choose'>Choose Image</label>
+
+    <input disabled={!account} type="file" id='file-upload' name='data' onChange={retrieveFile} />
+
+    <span className='textArea'>Image: {fileName}</span>
+
+    <button type='submit' className='upload'>Upload Image</button>
+
+    </form>
     
     </div>
 
